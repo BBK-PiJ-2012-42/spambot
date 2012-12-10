8c76f135ab21072809b6d038df5cc7b9a2db7cf0
Tom Nealon <tgnealon@gmail.com>
2012-12-10 13:25:11 +0000

    Initial commit.

---
+++ Spambot.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spambot;
+
+/**
+ *
+ * @author tnealo01
+ */
+public class Spambot {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
