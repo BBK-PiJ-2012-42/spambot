f8b7a8eaefd4d8540c767033e019f006be009f9f
Tom Nealon <tgnealon@gmail.com>
2012-12-10 13:37:46 +0000

    Initial commit.

--- Spambot.java
+++ Spambot.java
@@ -14,6 +14,11 @@ public class Spambot {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Spambot().launch();
     }
+    
+    private void launch() {
+        
+    }
+        
 }
