852839314e0d03b1af96c009d06c7da56635f134
Tom Nealon <tgnealon@gmail.com>
2012-12-11 14:45:32 +0000

    Worked on making the page parser more robust.

--- PageReader.java
+++ PageReader.java
@@ -13,15 +13,11 @@ import java.util.*;
  * @author tnealo01
  */
 public class PageReader implements WebPage {
-    private static int maxLinks = 50;
+    // Not implemented now returns as many links as it can find.
+    //private static int maxLinks = 50;
     private String url;
-    private BufferedReader bufferedText = null;
-    private InputStream myInput = null;
-    private Set<String> myEmailSet = null;
-    private Set<String> myLinkSet = null;
-    
-    final private String MAIL_REGEX = "([_A-Za-z0-9-]+)(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})";
-    final private String LINK_REGEX = "href=\"[^>]*\">";
+    // Current list of lines from the webpage.
+    private List<String> pageLines = new ArrayList<>();
     
     public PageReader(String url) {
         this.url = url;
@@ -32,54 +28,92 @@ public class PageReader implements WebPage {
     }
     
     public Set<String> getLinks() {
-        myLinkSet = new HashSet<>();
-        Scanner myScanner;
-        myScanner = new Scanner(this.getInputText());
-        String result;
-        boolean b = true;
-        while(myScanner.hasNext() == true) {
-            result = myScanner.findInLine(LINK_REGEX);
-            myLinkSet.add(result);
-            System.out.println(result);
-        }
-        return myLinkSet;     
+        if (pageLines.isEmpty()) {
+            pageLines = getPage();
+        } 
+        Set<String> myLinkSet = new HashSet<>(getListLinks(pageLines));
+        return myLinkSet;
     }
     
     public Set<String> getEmails() {
-        myEmailSet = new HashSet<>();
-        Scanner myScanner;
-        myScanner = new Scanner(this.getInputText());
-        String result;
-        boolean b = true;
-        while(b == true) {
-            result = myScanner.findInLine(MAIL_REGEX);
-            myEmailSet.add(result);
-            System.out.println(result);
-            if(result == null) {
-                b = false;
-            }
-        }
+        if (pageLines.isEmpty()) {
+            pageLines = getPage();
+        } 
+        Set<String> myEmailSet = new HashSet<>(getListEmails(pageLines));
         return myEmailSet;
     }
     
+    private List<String> getPage() {
+        // Get the page and return a nice list of lines
+        // for use by the link getter and email getter methods.
+        List<String> lines = new ArrayList<>();
+        BufferedReader bufferedReader;
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
+            String line;
+            do {
+                line = bufferedReader.readLine();
+                //System.out.println(line);
+                lines.add(line);
+            } while (line != null);  
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
     
-    
-    public InputStream getInputText() {
-    //    if(myInput.equals(null)) {
+    private List<String> getListLinks(List<String> lines) {
+        String result;
+        List<String> links = new ArrayList<>();
+        String linkRegex = "href=\"[^>]*\">";
+        for(String each : lines) {
             try {
-                myInput = new URL(url).openStream();
-                //InputStreamReader myInputStreamReader = new InputStreamReader(myInput);
-                //BufferedReader myBufferedReader = new BufferedReader(myInputStreamReader);
-                //bufferedText = myBufferedReader;
-                //return myBufferedReader;
-                return myInput;
-            } catch(IOException ex) {
-                ex.printStackTrace();
-                return null;
+                Scanner myScanner = new Scanner(each);
+                do {
+                    result = myScanner.findInLine(linkRegex);
+                    if (result != null) {
+                        // Remove the beginning and end href=""
+                        result = result.replaceFirst("href=\"", "");
+                        result = result.replaceFirst("\">*", "");
+                        // Remove anything left over after a space.
+                        result = result.replaceFirst(" .*", "");
+                        result = result.replaceFirst("&amp.*", "");
+                        // Still does not append http://domain to the links without it or
+                        // pass over emailto: and javascript: links.
+                        links.add(result);
+                        System.out.println(result);
+                    }
+                } while(result != null);
+            } catch (NullPointerException e) {
+                return links;
+               //e.printStackTrace(); 
             }
-      //  } else {
-      //      return myInput;
-      //  }
+        }      
+        return links;        
+    }
+    
+    private List<String> getListEmails(List<String> lines) {
+        List<String> emails = new ArrayList<>();
+        String result;
+        String mailRegex = "([_A-Za-z0-9-]+)(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})";
+        for(String each : lines) {
+            try {
+                Scanner myScanner = new Scanner(each);
+                do {
+                    result = myScanner.findInLine(mailRegex);
+                    if (result != null) {
+                        emails.add(result);
+                        System.out.println(result);
+                    }
+                } while(result != null);
+            } catch (NullPointerException e) {
+                return emails;
+               //e.printStackTrace(); 
+            }
+        }  
+        return emails;
     }
     
     
--- testclass.java
+++ testclass.java
@@ -4,29 +4,26 @@
  */
 package spambot;
 
-import java.util.Scanner;
-import java.util.regex.Pattern;
 
 /**
  *
  * @author tnealo01
  */
 public class testclass {
-    public static String testString = "<TD>E-Mail: <A HREF=\"mailto:nigel@dcs.bbk.ac.uk\">jim@dcs.bbk.ac.uk</a></TD> My research interests centre href=\"http://www.dcs.bbk.ac.uk/access_keys_desc.html\" on information management";
+    
+    // Run this class to see how PageReader works.
 
     public static void main(String[] args) {
-        PageGetter myGetter = new PageGetter("http://www.dcs.bbk.ac.uk/dcswiki/index.php/Main_Page");
-        myGetter.getLinks(myGetter.getPage());
-//        Pattern ValidURI;    
-//        final String MAIL_REGEX = "([_A-Za-z0-9-]+)(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})";
-//        final String LINK_REGEX = "href=\"[^>]*\">";
-//        Pattern htmltag = Pattern.compile("<a\\b[^>]*href=\"[^>]*>(.*?)</a>");
-//        Scanner myScanner = new Scanner(testString);
-//        String result = myScanner.findInLine(MAIL_REGEX);
-//        System.out.println(result);
-//        result = myScanner.findInLine(LINK_REGEX);
-//        System.out.println(result);
-//        result = myScanner.findInLine(htmltag);
-//        System.out.println(result);
+        PageReader myReader = new PageReader("http://www.dcs.bbk.ac.uk/dcswiki/index.php/Main_Page");
+        for (String link : myReader.getLinks()) {
+            System.out.println(link);
+        }
+        System.out.println("//////////////////////////////////////////////////////////");
+        System.out.println(myReader.getUrl());
+        System.out.println("//////////////////////////////////////////////////////////");
+        myReader = new PageReader("http://www.dcs.bbk.ac.uk/~nigel/");
+        for (String emails : myReader.getEmails()) {
+            System.out.println(emails);
+        }
     }
 }
