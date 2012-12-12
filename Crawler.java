5117d543b0dc18ebc176607768779b7f3f508f67
Tom Nealon <tgnealon@gmail.com>
2012-12-12 15:42:12 +0000

    yay works sort of!

--- Crawler.java
+++ Crawler.java
@@ -8,23 +8,43 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-public class Crawler {
-	private Set<String> links = new HashSet<>();
-	private Set<String> visitedLinks = new HashSet<>();
-	private Set<String> emails = new HashSet<>();
+public class Crawler implements Runnable {
+	private Set<String> links;
+	private Set<String> visitedLinks;
+	private Set<String> emails;
+        private String urlSeed;
+        private int threadNo;
+        
 	private Set<String> subLinkSet = new HashSet<>(); //for testing purposes
 	
-	public Crawler(String url) { //adds seed website upon construction of crawler.
-		links = new HashSet<>();
-		links.add(url);
+	public Crawler(Set<String> links,
+            Set<String> visitedLinks,
+            Set<String> emails,
+            String url,
+            int threadNo) { //adds seed website upon construction of crawler.
+                this.links = links;
+                this.visitedLinks = visitedLinks;
+                this.emails = emails;
+                this.urlSeed = url;
+                this.threadNo = threadNo;
+                links.add(urlSeed);
+                //this.run();
+	}
+        
+	public Crawler(Set<String> links,
+            Set<String> visitedLinks,
+            Set<String> emails,
+            int threadNo) { //adds seed website upon construction of crawler.
+                this.links = links;
+                this.visitedLinks = visitedLinks;
+                this.emails = emails;
+                this.threadNo = threadNo;
+                //this.run();
 	}
 	
-	public Crawler() { //used during testing only
-		super();
-	}
 	
 	
-	private void addLinks(Set<String> linkSubSet) {
+	private synchronized void addLinks(Set<String> linkSubSet, String element) {
                 Set<String> removeSet = new HashSet<>();
 		if (!visitedLinks.isEmpty()) {
 			Iterator<String> itr = linkSubSet.iterator();
@@ -38,40 +58,19 @@ public class Crawler {
                         
 		}		
                 links.addAll(linkSubSet);
+                addToVisitedSet(element);
 		
 	}
 	
 	
-	private void addEmails(Set<String> subEmailSet) {
-		if (emails == null) {
-			emails = new HashSet<String>();
-			emails.addAll(subEmailSet);
-		}
-		else {
-			emails.addAll(subEmailSet);
-		}
+	private synchronized void addEmails(Set<String> subEmailSet) {
+            emails.addAll(subEmailSet);
 	}
 	
-	private void addToVisitedSet(String url) {
-		if (visitedLinks == null) {
-			visitedLinks = new HashSet<String>();
-			visitedLinks.add(url);
-		}
-		else {
-			visitedLinks.add(url);
-		}
+	private synchronized void addToVisitedSet(String url) {
+            visitedLinks.add(url);
 	}			
 			
-	
-				
-	
-	public static void main(String[] args) {
-	
-		Crawler crawler = new Crawler();
-		crawler.launch();
-	}
-
-
         private synchronized void printVisited() {
         Iterator itr = visitedLinks.iterator();
         System.out.println("VISITED");
@@ -96,74 +95,24 @@ public class Crawler {
         }
         }
         
-	private void launch() {
-	
-		/**
-		* Test: OK
-		subLinkSet = new HashSet<String>();
-		subLinkSet.add("Hello");
-		subLinkSet.add("item1");
-		subLinkSet.add("item2");		
-		
-		this.addLinks(subLinkSet);
-		*/
+    @Override
+    public void run() {
+            System.out.println("Thread: "+ threadNo+ " running!");
+            Iterator<String> iterator = links.iterator(); //at this point iterator only has bbk.ac.uk, hence it gets reassigned at end of loop to include updated links.
+            while (iterator.hasNext() && PageReader.getCount() <= 500 && emails.size() <= 10) {		
+                String element = iterator.next();
+                System.out.println("Thread: "+ threadNo+ " ::: "+element); //test print
+                PageReader testReader = new PageReader(element);	
+                addEmails(testReader.getEmails());      
+                addLinks(testReader.getLinks(), element);
+                //addToVisitedSet(element);
+                links.remove(element);
+                iterator = links.iterator();
+            }
 
-		/**
-		* For each link in the main set, invokes a PageReader to visit the link and parse the page,
-		* while the set of links isn't empty and the number of sites visited is less than 51.
-		*/
-            
-                //subLinkSet = new HashSet<>();
-                //subLinkSet.add("https://sites.google.com/site/r3dt3ddy23/Home/dash");
-                subLinkSet.add("http://news.bbc.co.uk");
-                this.addLinks(subLinkSet);
-                
-                
-                
-//		Iterator<String> linkIterator = links.iterator();
-//                String nextUrl;
-//		while (linkIterator.hasNext() && PageReader.getCount() <= 50) {	
-//                        nextUrl = linkIterator.next();
-//			System.out.println(nextUrl); //test print
-//			PageReader pageReader = new PageReader(nextUrl);			
-//			addEmails(pageReader.getEmails()); 
-//			addLinks(pageReader.getLinks());
-//			addToVisitedSet(nextUrl);
-//                        linkIterator = links.iterator();
-//		}
-                
-                Iterator<String> iterator = links.iterator(); //at this point iterator only has bbk.ac.uk, hence it gets reassigned at end of loop to include updated links.
-                while (iterator.hasNext() && PageReader.getCount() <= 50 && emails.size() <= 5) {	
-                //while (iterator.hasNext()) {	
-                    String element = iterator.next();
-                    System.out.println(element); //test print
-                    PageReader testReader = new PageReader(element);	
-                    addEmails(testReader.getEmails());
-//                    
-//                    Iterator<String> emailIterator = testReader.getLinks().iterator();
-//                    while (emailIterator.hasNext()) {
-//                        System.out.println("Checking:::: " + emailIterator.next());
-//                    }
-                    
-                            
-                            
-                    addLinks(testReader.getLinks());
-                    addToVisitedSet(element);
-//                    this.printEmails();
-                    //this.printLinks();
-//                    this.printVisited();
-                    links.remove(element);
-                    iterator = links.iterator();
-                }
-                
-                System.out.println();
-                System.out.println("Gathered emails");
-                Iterator<String> emailIterator = emails.iterator();
-                while (emailIterator.hasNext()) {
-                    System.out.println(emailIterator.next());
-                }
-			
-	}
+
+
+    }
 }
 			
 	
--- Manager.java
+++ Manager.java
@@ -4,6 +4,10 @@
  */
 package spambot;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -11,7 +15,45 @@ import java.util.Set;
  * @author tnealo01
  */
 public class Manager {
-    PageReader myReader = new PageReader("http://www.dcs.bbk.ac.uk/~nigel/");
-    Set<String> mySet = myReader.getEmails();
+    private static Set<String> links = new HashSet<>();
+    private static Set<String> visitedLinks = new HashSet<>();
+    private static Set<String> emails = new HashSet<>();
+    
+    private String seed = "http://www.dcs.bbk.ac.uk";
+    
+    public static void main(String[] args) {
+        Manager myManager = new Manager();
+        myManager.spawnCrawlers(4);
+        printEmails();
+
+    }
+    
+    public static void printEmails() {
+        System.out.println();
+        System.out.println("Gathered emails");
+        Iterator<String> emailIterator = emails.iterator();
+        while (emailIterator.hasNext()) {
+            System.out.println(emailIterator.next());
+        }        
+    }
+    
+    public void spawnCrawlers(int threadCount) {
+        List<Thread> threadList = new ArrayList<>();
+        threadList.add(new Thread(new Crawler(links, visitedLinks, emails, seed, 0)));
+        for (int i = 1; i < threadCount; i++) {
+            threadList.add(new Thread(new Crawler(links, visitedLinks, emails, i)));
+        }
+        
+        
+        
+        for (Thread i : threadList) {
+            i.start();
+        }
+        
+//        new Thread(new Crawler(links, visitedLinks, emails, seed, 0)).start();
+//        new Thread(new Crawler(links, visitedLinks, emails, 1)).start();
+                
+    }
+    
     
 }
